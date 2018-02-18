927523e0e774febc475c2bbbaadb571e7fb74263
Daniel Ranchal <danitico98@gmail.com>
2018-02-18 11:43:56 +0100

    ejercicio2.sh terminado

--- P1/ejercicio2.sh
+++ P1/ejercicio2.sh
@@ -5,6 +5,17 @@ then
 else
 	let tamano=$2-1
 fi
-find $1 -size +$tamano | while read f
+echo "Nombre;LongitudUsuario;FechaModificacion;FechaAcceso;Tamano;Bloques;Permisos;Ejecutable"
+find $1 -size +$tamano\c -type f | while read f
 do
-	echo "
+	a="$(stat -c "%U" $f)"; a=${#a}
+	if [ -x $f ];
+	then
+		b=1
+	else
+		b=0
+	fi
+	echo `stat -c "$(basename $f);$a;%y;%X;%s;%b;%A;$b" $f` >> "ejercicio2tmp.txt"
+done
+cat ejercicio2tmp.txt | sort -t";" -n -k 5
+rm -rf ejercicio2tmp.txt
