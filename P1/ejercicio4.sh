7616b689f4af8e886a3ba9aadd4b136aba16a792
Daniel Ranchal <danitico98@gmail.com>
2018-02-18 17:56:57 +0100

    Terminado ejercicio4.sh

--- P1/ejercicio4.sh
+++ P1/ejercicio4.sh
@@ -3,9 +3,15 @@ echo "El numero de archivos en la carpeta actual es $(find . -maxdepth 1 -type f
 echo "Esta sería la lista de usuarios conectados en este momento: "
 echo "----------"
 echo "$(who -u | cut -d " " -f1 | uniq -u)"
-if ! read -p "¿Qué caracter quieres contar" -t5 caracter;
+if ! read -p "¿Qué caracter quieres contar? " -t5 caracter;
 then
 	caracter="a"
 fi
-echo "El caracter $caracter aparece $(find . -name "*$caracter*"
+let b=0
+find . -maxdepth 1 -type d,f -name "*$caracter*" | ( while read f
+do
+	a=$(echo $f | tr -cd $caracter | wc -c)
+	b=$[$a + $b]
+done
+echo "El caracter $caracter aparece $b veces en nombres de ficheros o carpetas contenidas en la carpeta actual" )
 
