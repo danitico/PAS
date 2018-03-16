88f72e15914419f7bf9aa52d8427955458146a89
Daniel@Uco <i62rapad@uco.es>
2018-03-16 13:14:51 +0100

    Ejercicio 4 hecho y ejercicio 2 reparado ya que habia bugs

--- P2/ejercicio2.sh
+++ P2/ejercicio2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat $1 | sed -r '/^\s*$/d; /^=*$/d; /^[[:space:]]/d; s/^\((.*)\)[[:space:]].*$/|-> Fecha de Estreno: \1/; s/^Reparto.(.*)/|-> Reparto: \1/; s/^Dirigida.por.(.*)/|-> Director: \1/; s/(^[1-9]hr.*)/Duración: \1/; s/(^[^|D].*)/Título: \1/'
+cat $1 | sed -r '/^\s*$/d; /^=*$/d; /^[[:space:]]/d; s/^\((.*)\)[[:space:]].*$/|-> Fecha de Estreno: \1/; s/^Reparto.(.*)/|-> Reparto: \1/; s/^Dirigida.por.(.*)/|-> Director: \1/; s/(^[1-9]hr.*)/|-> Duración: \1/; s/(^[^|].*)/Título: \1/'
--- P2/ejercicio4.sh
+++ P2/ejercicio4.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+if [ -e tmp.txt ]; then
+	rm -rf tmp.txt
+fi
+let milisegundos=$2*1000
 cat $1 |  while read ip
 do
-	comando=$(ping -c 1 -W 3 $ip)
-	if [ "$comando"  a.txt ];
-	then
-		echo "hey"
-	fi
+	ping -c 1 -W $2 $ip 2>&1 >/dev/null
+	if [ $? -eq 0 ]; then
+		ping -c 1 -W $2 $ip
+	else
+		echo "La IP $ip no respondió tras $milisegundos milisegundos"
+	fi >> tmp.txt
 done
+cat tmp.txt | grep -E 'time|no' | sed -r -e 's/^[0-9]*.bytes.from.([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*).*time=(.*)\s.*/La IP \1 respondió correctamente en \2 milisegundos/g' | sort -n -k 7
