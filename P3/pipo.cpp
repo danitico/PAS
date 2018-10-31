0e4e592e754f0736400070898919aa03670dd394
José Manuel Cuevas Muñoz <33511318+chema969@users.noreply.github.com>
2018-10-31 18:38:12 +0100

    Create pipo.cpp

---
+++ P3/pipo.cpp
@@ -0,0 +1,17 @@
+/*  _____ _____ _____   ____                      __                                          
+   |  __ \_   _|  __ \ / __ \                 __/_  `.  .-"""-.                                              
+   | |__) || | | |__) | |  | |               \_,` | \-'  /   )`-') 
+   |  ___/ | | |  ___/| |  | |                 "") `"`    \  ((`"`
+   | |    _| |_| |    | |__| |                ___Y  ,    .'7 /|
+   |_|   |_____|_|     \____/               (_,___/...-` (_/_/ sk
+                                                        
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+  cout<<"Voy contigo, Pipo (y o n):";
+  string pipo;
+  if(pipo=="y") cout<<"Voy contigo, Pipo"<<endl;
+  else cout<<"Pipo muere (NO)"<<endl;
+  }
